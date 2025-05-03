816ad5345b33f93e48b0f6ca005aa02c986c07a5
aqilahput <aqilahnuraisyahputri85@gmail.com>
2025-05-03 14:51:09 +0800

    implementasi kode

---
+++ Algoritma Divide and Conquer/BinarySearchTree.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    if key < root.val:
+        root.left = insert(root.left, key)
+    else:
+        root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+# Contoh penggunaan
+r = Node(50)
+r = insert(r, 30)
+r = insert(r, 20)
+r = insert(r, 40)
+r = insert(r, 70)
+r = insert(r, 60)
+r = insert(r, 80)
+
+found = search(r, 60)
+if found:
+    print("Elemen ditemukan:", found.val)
+else:
+    print("Elemen tidak ditemukan")
